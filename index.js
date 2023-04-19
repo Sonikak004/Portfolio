5d8331ca7b6f1fc9f10fc323f2db740f93108f6f
sonika <sonikak004@gmail.com>
2023-04-19 10:14:08 +0100

    Fix all Linter errors

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // hamburger menu open
 function open() {
-    document.querySelector('#mobile').style.display = 'block';
-  }
-  document.querySelectorAll('.items')[0].addEventListener('click', open);
-  
-  // hamburger menu close
-  
-  function close() {
-    document.getElementById('mobile').style.display = 'none';
-  }
-  document.getElementById('close-menu').addEventListener('click', close);
-  
+  document.querySelector('#mobile').style.display = 'block';
+}
+document.querySelectorAll('.items')[0].addEventListener('click', open);
+
+// hamburger menu close
+
+function close() {
+  document.getElementById('mobile').style.display = 'none';
+}
+document.getElementById('close-menu').addEventListener('click', close);
+
 // menu close when Items are clicked
-  
-  document.querySelectorAll('.menuItems').forEach((item) => {
-    item.addEventListener('click', close);
-  });
+
+document.querySelectorAll('.menuItems').forEach((item) => {
+  item.addEventListener('click', close);
+});
