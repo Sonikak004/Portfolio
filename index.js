bd9ad52009b35cd5effe77373ace47b8cf4b9d15
sonika <sonikak004@gmail.com>
2023-06-22 15:57:20 +0100

    Add Awesome books project to the 1st card with images

--- index.js
+++ index.js
@@ -22,37 +22,47 @@ document.querySelectorAll('.menuItems').forEach((item) => {
 const proCards = [
   {
     id: 0,
-    heading: 'Profesional Art Printing Data',
+    heading: 'Awesome Books',
     headingPop: 'Multi-post stories',
-    headingDesk: 'Keeping track of hundreds  of components website',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    headingDesk: 'Multi-post stories',
+    description: ' Manage your book collection effortlessly with Awesome Books app. Add and delete books with author names and titles with this SPA.',
     descriptionPop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     descriptionDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
     image: './images/probackground.png',
+    imagepopmob: './images/popup-img.png',
+    imagepopdesk: './images/popup-desktop-img.png',
     button: ['see live', 'see source', 'see project'],
-    technologies: ['html', 'Bootstrap', 'css', 'Ruby'],
-    liveLink: 'https://sonikak004.github.io/',
+    technologies: ['script', 'JavaScript', 'css', 'Ruby'],
+    liveLink: 'https://sonikak004.github.io/ES6-Awesome_Books/',
     sourceLink: 'https://github.com/Sonikak004/',
   },
   {
     id: 1,
-    heading: 'Profesional Art Printing Data',
-    headingDesk: 'profesional Art Printing Data',
-    descriptionPop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    descriptionDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    heading: 'profesional Art Printing Data',
+    headingPop: 'Awesome Books',
+    headingDesk: 'Awesome Books',
+    description1: 'daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    descriptionPop: 'Manage your book collection effortlessly with Awesome Books app. This single-page application (SPA) allows you to add and delete books, providing a seamless experience. Keep track of author names and titles, ensuring your collection is organized and easy to manage. Enjoy a user-friendly interface that simplifies the process of managing your books and enhances your reading experience.',
+    descriptionDesk: 'Easily and efficiently organize your entire book collection with the incredible Awesome Books application. This remarkable Single Page Application empowers you to effortlessly add delete and manage books while keeping track of crucial details such as author names and captivating titles. Experience the convenience of a user-friendly interface that enhances your reading journey making it an absolute delight to dive into your favorite literary treasures.',
     image: './images/probackground.png',
+    imagepopmob: './images/awesomebookimg.png',
+    imagepopdesk: './images/awesomebookimg.png',
     button: ['see live', 'see source', 'see project'],
-    technologies: ['html', 'Bootstrap', 'Ruby'],
-    liveLink: 'https://sonikak004.github.io/',
-    sourceLink: 'https://github.com/Sonikak004/',
+    technologies: ['html', 'Javascript', 'CSS'],
+    liveLink: 'https://sonikak004.github.io/ES6-Awesome_Books/',
+    sourceLink: 'https://github.com/Sonikak004/ES6-Awesome_Books',
   },
   {
     id: 2,
-    heading: 'Multi-post stories',
+    heading: 'Profesional Art Printing Data',
+    headingPop: 'Multi-post stories',
     headingDesk: 'Data Dashboard HealthCare',
+    description2: 'daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     descriptionPop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     descriptionDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
     image: './images/probackground.png',
+    imagepopmob: './images/popup-img.png',
+    imagepopdesk: './images/popup-desktop-img.png',
     button: ['see live', 'see source', 'see project'],
     technologies: ['html', 'Bootstrap', 'Ruby'],
     liveLink: 'https://sonikak004.github.io/',
@@ -61,10 +71,14 @@ const proCards = [
   {
     id: 3,
     heading: 'Profesional Art Printing Data',
+    headingPop: 'Multi-post stories',
     headingDesk: 'Website Portfolio',
+    description3: 'daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     descriptionPop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     descriptionDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
     image: './images/probackground.png',
+    imagepopmob: './images/popup-img.png',
+    imagepopdesk: './images/popup-desktop-img.png',
     button: ['see live', 'see source', 'see project'],
     technologies: ['html', 'Bootstrap', 'Ruby', 'Ruby on Rails'],
     liveLink: 'https://sonikak004.github.io/',
@@ -72,11 +86,15 @@ const proCards = [
   },
   {
     id: 4,
-    heading: 'Multi-post stories',
+    heading: 'Profesional Art Printing Data',
+    headingPop: 'Multi-post stories',
     headingDesk: 'Profesional Art Printing Data',
+    description4: 'daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     descriptionPop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     descriptionDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
     image: './images/probackground.png',
+    imagepopmob: './images/popup-img.png',
+    imagepopdesk: './images/popup-desktop-img.png',
     button: ['see live', 'see source', 'see project'],
     technologies: ['html', 'Bootstrap', 'Ruby'],
     liveLink: 'https://sonikak004.github.io/',
@@ -84,9 +102,13 @@ const proCards = [
   },
   {
     id: 5,
-    heading: 'Multi-post stories',
+    heading: 'Profesional Art Printing Data',
+    headingPop: 'Multi-post stories',
     headingDesk: 'Data Dashboard HealthCare',
+    description5: 'daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     image: './images/probackground.png',
+    imagepopmob: './images/popup-img.png',
+    imagepopdesk: './images/popup-desktop-img.png',
     descriptionPop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     descriptionDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
     button: ['see live', 'see source', 'see project'],
@@ -97,8 +119,12 @@ const proCards = [
   {
     id: 6,
     heading: 'Profesional Art Printing Data',
+    headingPop: 'Multi-post stories',
     headingDesk: 'Website Portfolio',
+    description6: 'daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     image: './images/probackground.png',
+    imagepopmob: './images/popup-img.png',
+    imagepopdesk: './images/popup-desktop-img.png',
     descriptionPop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     descriptionDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
     button: ['see live', 'see source', 'see project'],
@@ -120,12 +146,13 @@ function closeModal() {
 
 const popupVar = document.querySelector('#my-modal');
 
+for (let i = 0; i <= 6; i += 1) {
 let loaded = false;
 function getModal(dataId) {
   const popCard = `
   <div class="modal-content">
     <div class="modal-header">
-    <h3 class="modal-head-text dflex">${proCards[0].headingPop}</h3>
+    <h3 class="modal-head-text dflex">${proCards[dataId].headingPop}</h3>
     <h3 id="heading-popup" class="modal-head-text-desk dflex">${proCards[dataId].headingDesk}</h3>
       <span>
       <img src=images/vectorx.png alt="project image" class="close" id="popclose"/>
@@ -142,19 +169,19 @@ function getModal(dataId) {
     <nav class="icon-1">${proCards[dataId].technologies[2]}</nav>
     </li>
     <li class="pop-card4">
-    <nav class="icon-1">${proCards[3].technologies[3]}</nav>
+    <nav class="icon-1">${proCards[dataId].technologies[2]}</nav>
     </li>
     </ul>
     <div class="modal-body">
-    <img src=images/popup-img.png alt="project image" class="pop-window "/>
-    <img src=images/popup-desktop-img.png alt="project image" class="pop-window-desk"/>
+    <img src=${proCards[dataId].imagepopmob} alt="project image" class="pop-window "/>
+    <img src=${proCards[dataId].imagepopdesk} alt="project image" class="pop-window-desk"/>
     <p class="pop-text dflex"> ${proCards[dataId].descriptionPop}
     <p class="pop-text-desk dflex"> ${proCards[dataId].descriptionDesk}
     </div>
     <div class="modal-footer">
     <div class="pop-btn dflex">
     <button class="link-button1 ltext1" type="button">
-    <a class="seelive-but" href=${proCards[dataId].liveLink}>${proCards[dataId].button[1]}<img class="btn-icon1" src="./images/seelive.png"/>
+    <a class="seelive-but" href=${proCards[dataId].liveLink}>${proCards[dataId].button[0]}<img class="btn-icon1" src="./images/seelive.png"/>
     </a>
     </button>
     <button class="link-button2 ltext1" type="button">
@@ -173,7 +200,7 @@ function getModal(dataId) {
 
   if (loaded) popupVar.style.display = 'block';
   loaded = true;
-}
+}}
 
 getModal(0);
 
@@ -237,9 +264,9 @@ for (let i = 1; i <= 1; i += 1) {
 <div class="pro-img2-desk">
   <div/>
 <div class="pro2">
-<h2 class="ptext2 head-mob text-style">${proCards[0].heading}</h2>
+<h2 class="ptext2 head-mob text-style">${proCards[1].heading}</h2>
 <h2 class="ptext2 head-desk text-style">${proCards[5].headingDesk}</h2>
-<p class="ptext-2"> ${proCards[0].description}
+<p class="ptext-2"> ${proCards[1].description1}
 </p>
 <ul class="tech-button2">
   <li class="card1">
@@ -271,9 +298,9 @@ for (let i = 2; i <= 2; i += 1) {
 <div class="pro-img3-desk">
   <div/>
 <div class="pro3">
-<h2 class="ptext3 head-mob text-style">${proCards[0].heading}</h2>
+<h2 class="ptext3 head-mob text-style">${proCards[2].heading}</h2>
 <h2 class="ptext3 head-desk text-style">${proCards[3].headingDesk}</h2>
-<p class="ptext-2"> ${proCards[0].description}
+<p class="ptext-2"> ${proCards[3].description3}
 </p>
 <ul class="tech-button3">
   <li class="card1">
@@ -303,8 +330,8 @@ for (let i = 3; i <= 3; i += 1) {
   alt="project card four"
 />
 <div class="pro4">
-<h2 class="ptext4 text-style">${proCards[0].heading}</h2>
-<p class="ptext-2"> ${proCards[0].description}
+<h2 class="ptext4 text-style">${proCards[3].heading}</h2>
+<p class="ptext-2"> ${proCards[4].description4}
 </p>
 <ul class="tech-button4">
   <li class="card1">
@@ -336,9 +363,9 @@ for (let i = 4; i <= 4; i += 1) {
 <div class="pro-img5-desk">
   <div/>
 <div class="pro5">
-<h2 class="ptext5 head-mob text-style">${proCards[0].heading}</h2>
+<h2 class="ptext5 head-mob text-style">${proCards[4].heading}</h2>
 <h2 class="ptext5 head-desk text-style">${proCards[5].headingDesk}</h2>
-<p class="ptext-2"> ${proCards[0].description}
+<p class="ptext-2"> ${proCards[5].description5}
 </p>
 <ul class="tech-button5">
   <li class="card1">
@@ -370,9 +397,9 @@ for (let i = 5; i <= 5; i += 1) {
 <div class="pro-img6-desk">
   <div/>
 <div class="pro6">
-<h2 class="ptext6 head-mob text-style">${proCards[0].heading}</h2>
+<h2 class="ptext6 head-mob text-style">${proCards[5].heading}</h2>
 <h2 class="ptext6 head-desk text-style">${proCards[3].headingDesk}</h2>
-<p class="ptext-2"> ${proCards[0].description}
+<p class="ptext-2"> ${proCards[6].description6}
 </p>
 <ul class="tech-button6">
   <li class="card1">
